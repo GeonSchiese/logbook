7745571f74db3c3201da2491d7851892ab2307a7
GeonSchiese <90725930+GeonSchiese@users.noreply.github.com>
2022-03-11 16:04:43 +0100

    Add .env File functionality

--- client/src/apis/weather.js
+++ client/src/apis/weather.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+//.env Datei von hier: https://stackoverflow.com/questions/48699820/how-do-i-hide-api-key-in-create-react-app
+
 export default axios.create({
     baseURL: 'https://avwx.rest/api/',
     timeout: 5000,
-    headers: {"Authorization": ""}
+    headers: {"Authorization": process.env.REACT_APP_WEATHER_API_KEY}
 });
